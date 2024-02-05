6cf0bbc920d7b20a346ea7cc2ac6451222bc5f51
İlker Sipahi <97413177+sipahilker@users.noreply.github.com>
2024-02-05 10:45:51 +0300

    Add files via upload

---
+++ TSQL-2.sql
@@ -0,0 +1,5 @@
+--select * from film where replacement_cost BETWEEN 12.99 AND 16.99;
+
+--select * from actor where first_name IN ('Penelope', 'Nick', 'Ed') and last_name IN ('Penelope', 'Nick', 'Ed');
+
+--select * from film where rental_rate IN (0.99, 2.99, 47) AND replacement_cost IN (19.99, 15.99, 28.99);
